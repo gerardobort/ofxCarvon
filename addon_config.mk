19e4612522d8368008d877cdbc2dca56e3a0cfcf
Gerardo Bort <gbort@medallia.com>
2017-04-01 23:07:05 -0300

    this just works ;)

--- addon_config.mk
+++ addon_config.mk
@@ -4,3 +4,9 @@ meta:
     ADDON_AUTHOR = Gerardo Bort
     ADDON_TAGS = computer vision, cemrera, fisheye
     ADDON_URL = http://github.com/gerardobort/ofxCarvon
+
+
+common:
+	# requires opencv with opencl support installed
+	USER_CFLAGS = -I/usr/local/include/opencv -I/usr/local/include 
+	USER_LDFLAGS = /usr/local/lib/libopencv_ocl.dylib
