50e79f29e3ae52c8104ed5bf991e79d1211e312e
Gerardo Bort <gerardobort@gmail.com>
2017-04-11 03:22:16 -0300

    not compiling yet, getting closer

--- addon_config.mk
+++ addon_config.mk
@@ -11,5 +11,11 @@ common:
     # #include "/usr/local/include/opencv2/opencv.hpp"  --->  ~/Applications/openFrameworks/libs/openFrameworks/utils/ofConstants.h
 
 	# requires opencv with opencl support installed
-	USER_CFLAGS = -I/usr/local/include/opencv -I/usr/local/include 
-	USER_LDFLAGS = /usr/local/lib/libopencv_calib3d.dylib /usr/local/lib/libopencv_ocl.dylib
+	USER_CFLAGS = -I/usr/local/include/opencv -I/usr/local/include/opencv2
+	USER_LDFLAGS = /usr/local/lib/libopencv_calib3d.dylib /usr/local/lib/libopencv_contrib.dylib /usr/local/lib/libopencv_core.dylib /usr/local/lib/libopencv_features2d.dylib /usr/local/lib/libopencv_flann.dylib /usr/local/lib/libopencv_gpu.dylib /usr/local/lib/libopencv_highgui.dylib /usr/local/lib/libopencv_imgproc.dylib /usr/local/lib/libopencv_legacy.dylib /usr/local/lib/libopencv_ml.dylib /usr/local/lib/libopencv_nonfree.dylib /usr/local/lib/libopencv_objdetect.dylib /usr/local/lib/libopencv_ocl.dylib /usr/local/lib/libopencv_photo.dylib /usr/local/lib/libopencv_stitching.dylib /usr/local/lib/libopencv_superres.dylib /usr/local/lib/libopencv_video.dylib /usr/local/lib/libopencv_videostab.dylib 
+
+    # avoid ofxCv libs to be loaded, give place to brew's includes
+    ADDON_LIBS_EXCLUDE = libs/opencv
+    ADDON_LIBS_EXCLUDE += libs/opencv/%
+    ADDON_INCLUDES_EXCLUDE = libs/opencv
+    ADDON_INCLUDES_EXCLUDE += libs/opencv/%
